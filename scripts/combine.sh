5d07ca5a9bcc874bc6c4672c02b7d349c4ecb433
Spencer Gunning <swcgunning@gmail.com>
2022-03-28 12:13:26 -0400

    Set program variables in UNIX style

--- scripts/combine.sh
+++ scripts/combine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Set program variables
 pdftk="/c/Program Files (x86)/PDFtk/bin/pdftk.exe"
 
 # Get name of month
@@ -8,7 +9,7 @@ month=$(date --date="$(date) + 14 day" +%B)
 fdate="$year$month"
 
 # Combine PDFs
-pdftk "out/newsletter-front_$fdate.pdf" "out/newsletter-back_$fdate.pdf" cat output "out/newsletter-$fdate.pdf"
+"$pdftk" "out/newsletter-front_$fdate.pdf" "out/newsletter-back_$fdate.pdf" cat output "out/newsletter-$fdate.pdf"
 
 # Delete Individual PDFs
 rm "out/newsletter-front_$fdate.pdf" "out/newsletter-back_$fdate.pdf"
